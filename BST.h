1af143fd7f9f91c0597356a551888b8f941e2ea8
R. Connor Franckowiak <rcf97@outlook.com>
2019-09-26 17:49:59 -0600

    Update BST.h

--- BST.h
+++ BST.h
@@ -11,39 +11,12 @@ class BST : public BSTInterface {
 public:
 	BST();
 	~BST();
-
-	//Please note that the class that implements this interface must be made
-	//of objects which implement the NodeInterface
-
-	/*
-	* Returns the root node for this tree
-	*
-	* @return the root node for this tree.
-	*/
 	NodeInterface * getRootNode() const;
-
-	/*
-	* Attempts to add the given int to the BST tree
-	*
-	* @return true if added
-	* @return false if unsuccessful (i.e. the int is already in tree)
-	*/
 	bool add(int data);
 	bool insert(Node* &localRoot, int data);
-
-	/*
-	* Attempts to remove the given int from the BST tree
-	*
-	* @return true if successfully removed
-	* @return false if remove is unsuccessful(i.e. the int is not in the tree)
-	*/
 	bool search(Node* localRoot, int data) const;
 	bool remove(int data);
 	bool delVal(Node*& localRoot, int data);
-
-	/*
-	* Removes all nodes from the tree, resulting in an empty tree.
-	*/
 	void delTree(Node*& localRoot);
 	void clear();
 };
